400a2d0c5afdc70e794620425bb3f4b65dda1066
Anna <anna.andrianova123@gmail.com>
2022-10-31 05:43:06 +0000

    146.1

---
+++ session/146.1.php
@@ -0,0 +1,8 @@
+<?php
+	session_start();
+?>
+<a href='146.1.php'> Инфо </a>
+<form action = '146.1.php' method ='GET'>
+        <input type = 'text' name='x'><br>
+        <input type = 'text' name='y'><br>
+        <input type="submit">
