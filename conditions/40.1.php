c68ac30a22a6b5693772ce06ca438ea00f7b1486
Anna <anna.andrianova123@gmail.com>
2022-11-01 14:43:26 +0000

    40.1

---
+++ conditions/40.1.php
@@ -0,0 +1,19 @@
+<?php
+	$num = 1;
+	
+	if ($num === 1) {
+		echo 'зима';
+	} 
+    elseif ($num === 2) {
+		echo 'весна';
+	}
+    elseif ($num === 3) {
+		echo 'лето';
+	} 
+    elseif ($num === 4) {
+		echo 'осень';
+	} 
+    else {
+		echo 'неверные параметры';
+	}
+?>
