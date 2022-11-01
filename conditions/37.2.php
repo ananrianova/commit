aafe1e1307667d82a1b776de64f59878ba07c23b
Anna <anna.andrianova123@gmail.com>
2022-11-01 14:20:56 +0000

    37.2

---
+++ conditions/37.2.php
@@ -0,0 +1,15 @@
+<?php
+	$day = 1; 
+
+	if ($day > 0 and $day <= 10) {
+		echo 'Первая';
+	}
+	
+	if ($day > 10 and $day <= 20) {
+		echo 'Вторая';
+	}
+	
+	if ($day > 20 and $day <= 31) {
+		echo 'Третья'; 
+	}
+?>
