9134ac642553ebf84ed1b3295179ef787893caf1
Anna <anna.andrianova123@gmail.com>
2022-11-02 17:40:54 +0000

    63.2

---
+++ cycles/63.2.php
@@ -0,0 +1,18 @@
+<?php
+	$arr = [
+		'employee1' => 100,
+		'employee2' => 200,
+		'employee3' => 300,
+		'employee4' => 400,
+		'employee5' => 500,
+		'employee6' => 600,
+		'employee7' => 700,
+	];
+    foreach ($arr as $key => $elem) 
+    if ($elem <= 400)
+    {
+        $arr[$key] *= 1.1;
+    }
+    foreach ($arr as $key => $elem) {
+       echo $elem.'<br>';}
+?>
