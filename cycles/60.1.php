623fda386d473ba30d117ec1c58acff7166f0d8c
Anna <anna.andrianova123@gmail.com>
2022-11-02 16:35:31 +0000

    60.1

--- cycles/60.1.php
+++ cycles/60.1.php
@@ -1,9 +1,16 @@
 <?php
-	$arr = [];
+$arr = [];
 	
-	for ($i = 0; $i <= 10; $i++) {
-		$arr[] = $i + 1;
-	}
-	
-	var_dump($arr);
+	$arr[] = 1;
+	$arr[] = 2;
+	$arr[] = 3;
+	$arr[] = 4;
+	$arr[] = 5;
+	$arr[] = 6;
+	$arr[] = 7;
+	$arr[] = 8;
+	$arr[] = 9;
+	$arr[] = 10;
+
+echo '<br>';
 ?>
