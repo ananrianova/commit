f5fb7e248b2bf52eeeced91a7c7c9a83bf60f7bf
Anna <anna.andrianova123@gmail.com>
2022-11-09 13:53:19 +0000

    car_1

---
+++ func/car_1.php
@@ -0,0 +1,16 @@
+<?php
+class Car {
+public $marka;
+public $color;
+
+public function go () {
+    echo 'Вперед <br>';
+}
+public function turn () {
+    echo 'Поворот <br>';
+}
+public function stop () {
+    echo 'Остановка <br>';
+}
+}
+?>
