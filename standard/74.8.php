cbc6bacc253b0b1d67bffd030cfba569eb5d4a55
Anna <anna.andrianova123@gmail.com>
2022-11-30 10:26:04 +0000

    74.6

---
+++ standard/74.8.php
@@ -0,0 +1,9 @@
+<?php
+$arr = [];
+
+for ($i=0; $i < 10; $i++) {
+	$sluch=mt_rand();
+	$arr[]=$sluch;
+}
+var_dump ($arr);
+?>
---
+++ standard/74.9.php
@@ -0,0 +1,6 @@
+<?php
+$a=20;
+$b=12;
+$abs=abs($a-$b);
+echo $abs;
+?>
